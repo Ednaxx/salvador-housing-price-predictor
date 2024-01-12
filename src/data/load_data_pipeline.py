bb35fde2e909f5307d72758756f818d4677281ea
Alexandre Morais <xandre2setembro2002@hotmail.com>
2024-01-12 18:54:19 -0300

    Loaded data

--- src/data/load_data_pipeline.py
+++ src/data/load_data_pipeline.py
@@ -2,18 +2,19 @@ from extract.extract import get_data
 from transform.transform import transform_data
 import pandas as pd
 
-def load_data():
-    scraped_data = get_data(1, 101)
+def load_data(i):
+    scraped_data = get_data((1 + (i * 50)), (51 + (i * 50)))
     clean_data = transform_data(scraped_data)
 
     return clean_data
 
 if __name__ == "__main__":
-    try:
-        df = pd.read_csv("./data/housing_data.csv")
-        df = pd.concat([df, load_data()], axis=0)
-        df = df.drop_duplicates(subset="id")
-    except:
-        df = load_data()
-    
-    df.to_csv("./data/housing_data.csv", index=False)
+    for i in range(5):
+        try:
+            df = pd.read_csv("./data/housing_data.csv")
+            df = pd.concat([df, load_data(i)], axis=0)
+            df = df.drop_duplicates(subset="id")
+        except:
+            df = load_data()
+        
+        df.to_csv("./data/housing_data.csv", index=False)
