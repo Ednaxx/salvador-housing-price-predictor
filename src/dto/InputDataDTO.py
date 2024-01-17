c4e8d5fccf41ea08a0214c9c2edbd60aa8d98b07
Alexandre Morais <xandre2setembro2002@hotmail.com>
2024-01-17 12:58:05 -0300

    Set API validation for housing type

--- src/dto/InputDataDTO.py
+++ src/dto/InputDataDTO.py
@@ -1,12 +1,17 @@
 from pydantic import BaseModel
 from typing import List
+from enum import Enum
+
+class HousingTypes(str, Enum):
+        house = "house"
+        apartment = "apartment"
 
 class Housing(BaseModel):
         areas: float | None = None
         bedrooms: int | None = None
         bathrooms: int | None = None
         parkingSpots: int | None = None
-        type: str | None = None
+        type: HousingTypes
         neighborhood: str | None = None
 
 class InputDataDTO(BaseModel):
